3c4840493faec1e34643d369ed8abfc31d18e1dc
bruno <brunoalbuquerquebrito@outlook.com>
2019-01-15 02:54:26 -0200

    Criando modelos

---
+++ src/main/java/com/br/startmeup/models/Agenda.java
@@ -0,0 +1,34 @@
+package com.br.startmeup.models;
+
+public class Agenda {
+
+    private int Id;
+
+    private String nome;
+
+    private String descricao;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/com/br/startmeup/models/Evento.java
@@ -0,0 +1,56 @@
+package com.br.startmeup.models;
+
+import java.util.Date;
+
+public class Evento {
+
+    private int id;
+
+    private String nome;
+
+    private String endereco;
+
+    private String local;
+
+    private Date data;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
